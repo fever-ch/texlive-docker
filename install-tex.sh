e6245fbba41accf377c978ed49ac9c8009280915
Raphaël P. Barazzutti <raphael@barazzutti.net>
2019-03-12 00:06:38 +0100

    tweaking wget

--- install-tex.sh
+++ install-tex.sh
@@ -4,14 +4,23 @@ export TERM=xterm
 
 echo "Installing TeXlive $YEAR"
 
+mv /usr/bin/wget /usr/bin/wget.orig
+
+echo -e "#/bin/bash\n/usr/bin/wget.orig \"\$@\" || (sleep 10; /usr/bin/wget \"\$@\")" > /usr/bin/wget
+
+
+cat /usr/bin/wget
+chmod +x /usr/bin/wget
+
+
 if (( $YEAR < 2018 )); then
-	wget -c ftp://tug.org/historic/systems/texlive/$YEAR/tlnet-final/install-tl.zip || exit -1
+	wget -o /dev/stdout -c ftp://tug.org/historic/systems/texlive/$YEAR/tlnet-final/install-tl.zip || exit -1
 
 	unzip install-tl.zip
 
 	rm install-tl.zip
 else
-	wget -c ftp://tug.org/historic/systems/texlive/$YEAR/install-tl-unx.tar.gz || exit -1
+	wget -o /dev/stdout  -c ftp://tug.org/historic/systems/texlive/$YEAR/install-tl-unx.tar.gz || exit -1
 
 	tar xvfz install-tl-unx.tar.gz
 	
@@ -27,3 +36,5 @@ fi;
 
 cd .. ; rm -rf install-tl-*
 
+rm cat /usr/bin/wget
+mv cat /usr/bin/wget.orig cat /usr/bin/wget
