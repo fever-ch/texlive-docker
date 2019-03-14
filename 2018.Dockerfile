d0ddefad908c3a03f1e49c3e7254f92d6c1e8f61
Raphaël P. Barazzutti <raphael@barazzutti.net>
2019-03-14 11:01:02 +0100

    trying out other servers

--- 2018.Dockerfile
+++ 2018.Dockerfile
@@ -1,3 +1,4 @@
 FROM feverch/texlive:base
+ARG MIRROR="ftp://tug.org/historic/systems/texlive"
 RUN cd /tmp && ./install-tex.sh 2018 full && rm ./install-tex.sh
 ENV PATH="${PATH}:/usr/local/texlive/2018/bin/x86_64-linux/latex"
--- install-tex.sh
+++ install-tex.sh
@@ -2,14 +2,14 @@
 export YEAR=$1
 export SCHEME=$2
 
-echo "Installing TeXlive $YEAR"
+echo "Installing TeXlive $YEAR with mirror $MIRROR"
 
 echo "Setting up a \"defensive\" wget wrapper"
 mv /usr/bin/wget /usr/bin/wget.orig	
 echo -e "#/bin/bash\n/usr/bin/wget.orig \"\$@\" || (sleep 5; /usr/bin/wget \"\$@\")" > /usr/bin/wget	
 chmod +x /usr/bin/wget
 
-wget --tries=10 --timeout=30 -q --user-agent=texlive/wget -c ftp://tug.org/historic/systems/texlive/$YEAR/tlnet-final/install-tl-unx.tar.gz || exit -1
+wget --tries=10 --timeout=30 -q --user-agent=texlive/wget -c $MIRROR/$YEAR/tlnet-final/install-tl-unx.tar.gz || exit -1
 
 mkdir /install-tl-unx
 
@@ -18,7 +18,7 @@ tar xvfz install-tl-unx.tar.gz -C /install-tl-unx --strip-components=1
 
 echo -e "selected_scheme scheme-$SCHEME" >> /install-tl-unx/texlive.profile
 
-/install-tl-unx/install-tl -profile /install-tl-unx/texlive.profile -repository ftp://tug.org/historic/systems/texlive/$YEAR/tlnet-final || exit -1
+/install-tl-unx/install-tl -profile /install-tl-unx/texlive.profile -repository $MIRROR/$YEAR/tlnet-final || exit -1
 
 rm -rf /install-tl-unx
 
